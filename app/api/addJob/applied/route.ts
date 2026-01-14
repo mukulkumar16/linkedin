7909c0b965b9bb3e4e95c49a3f1475a34ca19182
mukul kumar <mukulkumarofficial5@gmail.com>
2026-01-15 01:00:53 +0530

    second commit

--- app/api/addJob/applied/route.ts
+++ app/api/addJob/applied/route.ts
@@ -1,20 +1,24 @@
+// app/api/addJob/applied/route.ts
 import { currentUser } from "@clerk/nextjs/server";
 import prisma from "@/helper/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+  // Get current user
   const user = await currentUser();
   if (!user) return NextResponse.json({ applied: false });
 
+  // Find user in DB
   const dbUser = await prisma.user.findUnique({
     where: { clerkId: user.id },
   });
-
   if (!dbUser) return NextResponse.json({ applied: false });
 
+  // Get jobId from query
   const jobId = req.nextUrl.searchParams.get("jobId");
   if (!jobId) return NextResponse.json({ applied: false });
 
+  // Check if applied
   const applied = await prisma.jobApplication.findUnique({
     where: {
       jobId_userId: {
