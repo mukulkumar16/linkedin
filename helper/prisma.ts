f69d6928d28335dfcd065c5db8fc9547a9b526e3
mukul kumar <mukulkumarofficial5@gmail.com>
2026-01-15 21:02:48 +0530

    second commit

--- helper/prisma.ts
+++ helper/prisma.ts
@@ -1,18 +1,14 @@
+// helper/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
+  prisma: PrismaClient | undefined;
 };
 
-export const prisma =
+const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ["error"],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
   });
 
 if (process.env.NODE_ENV !== "production") {
